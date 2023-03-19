a52bc60881ebbbd4900e38dda40eea7c3f9eb6ec
Dexter Ryan Floreza <deryanfloreza@outlook.com>
2023-03-19 03:35:11 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # discord token
-Token = "MTA4Njg2MDUyNjA1MjcyNDc4Ng.GJ2MH4.oSx2Ih-ILG2eghQfaskKstRau5rI9irOlla7M4"
+Token = ""
 
 # this is the CHAT GPT API KEY
-API_KEY = "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"
+API_KEY = ""
