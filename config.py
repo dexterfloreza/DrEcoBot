dc4b0c11d6a5e606cd67a75dd1f8f26f7ea618a2
Dexter Ryan Floreza <deryanfloreza@outlook.com>
2023-03-19 00:52:57 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# discord token
+Token = "MTA4Njg2MDUyNjA1MjcyNDc4Ng.GJ2MH4.oSx2Ih-ILG2eghQfaskKstRau5rI9irOlla7M4"
+
+# this is the CHAT GPT API KEY
+API_KEY = "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"
